065d952d9d4d32174b80ea07c7a4e107df3c17ea
spencer <spencer.bigum@gmail.com>
2017-05-18 02:57:14 -0400

    added new start script to package.json

--- routes/index.js
+++ routes/index.js
@@ -2,15 +2,6 @@ const express = require('express')
 const router = express.Router()
 const storeCtrl = require('../controllers/storeCtrl.js')
 
-// Do work here
-router.get('/', (req, res) => {
-  res.send('no access')
-  // res.send({
-  //   text:'Hey! It works!'
-  // });
-  // res.render('layout')
-})
-
 // chain functions for uploading images -> data
 router.post('/add', storeCtrl.upload, storeCtrl.resize, storeCtrl.createStore)
 router.get('/store/:slug', storeCtrl.getStore)
@@ -22,5 +13,13 @@ router.post(
   storeCtrl.resize,
   storeCtrl.updateStore
 )
+// Do work here
+router.get('/', (req, res) => {
+  res.send('no access')
+  // res.send({
+  //   text:'Hey! It works!'
+  // });
+  // res.render('layout')
+})
 
 module.exports = router
